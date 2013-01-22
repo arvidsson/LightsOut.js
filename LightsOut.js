9b7f761d89bf468828d9fcd4d951cb17030ad6c3
Pär Arvidsson <arvidsson@gmail.com>
2013-01-22 19:02:27 +0100

    experimentation

--- LightsOut.js
+++ LightsOut.js
@@ -1,147 +1,165 @@
-function LightsOut() {
-    this.width = 800;
-    this.height = 480;
-    this.maxRandomLights = 15;
-    this.hasWon = false;
-    this.lights = [];
-};
-
-LightsOut.prototype = {
-    init: function() {
-        this.lights = new Array(5);
-        for (i = 0; i < 5; i++) {
-            this.lights[i] = new Array(5);
-            for (j = 0; j < 5; j++)
-                this.lights[i][j] = false;
-        }
-        this.reset();
-    },
-    
-    reset: function() {
-        var solvable = false;
-        while (!solvable) {
-            // turn off all lights
-            for (i = 0; i < 5; i++)
-                for (j = 0; j < 5; j++)
-                    this.lights[i][j] = false;
-            
-            // turn on random lights
-            var total = Math.floor((Math.random() * this.maxRandomLights) + 1);
-            while (total > 0) {
-                var i = Math.floor(Math.random() * 5);
-                var j = Math.floor(Math.random() * 5);
-                if (!this.lights[i][j]) {
-                    this.lights[i][j] = true;
-                    total--;
-                }
-            }
-            
-            // check if the board can be solved
-            solvable = this.isSolvable();
-        }
-    },
-    
-    isSolvable: function() {
-        // check quiet pattern #1
-        var total = 0;
-        for (i = 0; i < 5; i++)
-            if (i != 2)
-                for (j = 0; j < 5; j += 2)
-                    if (this.lights[i][j])
-                        total++;
-        if (total % 2 != 0)
-            return false;
-        
-        // check quiet pattern #2
-        total = 0;
-        for (i = 0; i < 5; i++)
-            if (i != 2)
-                for (j = 0; j < 5; j += 2)
-                    if (this.lights[j][i])
-                        total++;
-        if (total % 2 != 0)
-            return false;
-        
-        return true;
-    },
-    
-    update: function(context, mouseX, mouseY) {
-        // check if we have won, so we can display a congratulatory message
-        if (this.hasWon) {
-            this.hasWon = false;
-            this.reset();
-            return this.render(context);
-        }
-        
-        // find out which light we have clicked on
-        var x = -1;
-        var y = -1;
-        for (i = 0; i < 5; i++) {
-            for (j = 0; j < 5; j++) {
-                var mx = j * 55 + 265;
-                var my = i * 55 + 105;
-                if (mouseX > mx && mouseX < (mx + 50) && mouseY > my && mouseY < (my + 50)) {
-                    x = j;
-                    y = i;
-                }
-            }
-        }
-    
-        // toggle lights
-        if (x != -1 && y != -1) {
-            this.lights[y][x] = !this.lights[y][x];
-            if (x > 0)
-                this.lights[y][x-1] = !this.lights[y][x-1]; // toggle left
-            if (x < 4)
-                this.lights[y][x+1] = !this.lights[y][x+1]; // toggle right
-            if (y > 0)
-                this.lights[y-1][x] = !this.lights[y-1][x]; // toggle above
-            if (y < 4)
-                this.lights[y+1][x] = !this.lights[y+1][x]; // toggle below
-        }
-        
-        // check for win!
-        var total = 0;
-        for (i = 0; i < 5; i++)
-            for (j = 0; j < 5; j++)
-                if (this.lights[i][j])
-                    total++;
-        if (total < 1)
-            this.hasWon = true;
-        
-        this.render(context);
-    },
-    
-    render: function(context) {
-        context.save();
-        context.scale(window.scaleFactor, window.scaleFactor);
-    
-        // clear the screen
-        context.beginPath();
-        context.rect(0, 0, this.width, this.height);
-        context.translate(0, 0);
-        context.fillStyle = "rgb(0,0,0)";
-        context.fill();
-
-        // draw the lights
-        for (i = 0; i < 5; i++) {
-            for (j = 0; j < 5; j++) {
-                var x = j * 55 + 265;
-                var y = i * 55 + 105;
-                context.fillStyle = "rgb(65, 65, 65)";
-                if (this.lights[i][j])
-                    context.fillStyle = "rgb(255, 255, 255)";
-                context.fillRect(x, y, 50, 50);
-            }
-        }
-        
-        if (this.hasWon) {
-            context.fillStyle = '#fff';
-            context.font = 'normal bold 16px sans-serif';
-            context.textBaseline = 'bottom';
-            context.fillText('You win!', 367, 75);
-        }
-        
-        context.restore();
+(function(g, undefined) {
+  
+  var width = 800,
+      height = 480,
+      scalef = 1.0,
+      maxRandomLights = 15,
+      hasWon = false,
+      lights = [];
+  
+  g.init = function() {
+    lights = new Array(5);
+    for (i = 0; i < 5; i++) {
+      lights[i] = new Array(5);
+        for (j = 0; j < 5; j++)
+          lights[i][j] = false;
     }
-};
+    reset();
+    
+    window.onresize = function() {
+      scalef = Math.min(window.innerWidth / width, window.innerHeight / height);
+      window.canvas.width = Math.floor(width * scalef);
+      window.canvas.height = Math.floor(height * scalef);
+      g.render(window.context);
+    };
+    
+    window.canvas = document.getElementById("theCanvas");
+    scalef = Math.min(window.innerWidth / width, window.innerHeight / height);
+    window.canvas.width = Math.floor(width * scalef);
+    window.canvas.height = Math.floor(height * scalef);
+    window.context = canvas.getContext("2d");
+    window.canvas.addEventListener('click', function(e) {
+      // translate mouse coordinates to canvas resolution before we pass them along
+      var rect = window.canvas.getBoundingClientRect();
+      var x = (e.clientX - rect.left) / (window.innerWidth / width);
+      var y = (e.clientY - rect.top) / (window.innerHeight / height);
+      g.update(window.context, x, y);
+    }, false);
+    g.render(window.context);
+  };
+  
+  var reset = function() {
+    do {
+      // turn off all lights
+      for (i = 0; i < 5; i++)
+        for (j = 0; j < 5; j++)
+          lights[i][j] = false;
+            
+      // turn on random lights
+      var total = Math.floor((Math.random() * maxRandomLights) + 1);
+      while (total > 0) {
+        var i = Math.floor(Math.random() * 5);
+        var j = Math.floor(Math.random() * 5);
+        if (!lights[i][j]) {
+          lights[i][j] = true;
+          total--;
+        }
+      }
+    } while (!solvable());
+  };
+  
+  var solvable = function() {
+    // check quiet pattern #1
+    var total = 0;
+    for (i = 0; i < 5; i++)
+      if (i != 2)
+        for (j = 0; j < 5; j += 2)
+          if (lights[i][j])
+            total++;
+    if (total % 2 != 0)
+      return false;
+        
+    // check quiet pattern #2
+    total = 0;
+    for (i = 0; i < 5; i++)
+      if (i != 2)
+        for (j = 0; j < 5; j += 2)
+          if (lights[j][i])
+            total++;
+    if (total % 2 != 0)
+      return false;
+      
+    return true;
+  };
+  
+  g.update = function(context, mouseX, mouseY) {
+    // check if we have won, so we can display a congratulatory message
+    if (hasWon) {
+      hasWon = false;
+      reset();
+      return g.render(context);
+    }
+        
+    // find out which light we have clicked on
+    var x = -1;
+    var y = -1;
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        var mx = j * 55 + 265;
+        var my = i * 55 + 105;
+        if (mouseX > mx && mouseX < (mx + 50) && mouseY > my && mouseY < (my + 50)) {
+          x = j;
+          y = i;
+        }
+      }
+    }
+    
+    // toggle lights
+    if (x != -1 && y != -1) {
+      lights[y][x] = !lights[y][x];
+      if (x > 0)
+        lights[y][x-1] = !lights[y][x-1]; // toggle left
+      if (x < 4)
+        lights[y][x+1] = !lights[y][x+1]; // toggle right
+      if (y > 0)
+        lights[y-1][x] = !lights[y-1][x]; // toggle above
+      if (y < 4)
+        lights[y+1][x] = !lights[y+1][x]; // toggle below
+    }
+        
+    // check for win!
+    var total = 0;
+    for (i = 0; i < 5; i++)
+      for (j = 0; j < 5; j++)
+        if (lights[i][j])
+          total++;
+    if (total < 1)
+      hasWon = true;
+        
+    g.render(context);
+  };
+    
+  g.render = function(context) {
+    context.save();
+    context.scale(scalef, scalef);
+  
+    // clear the screen
+    context.beginPath();
+    context.rect(0, 0, width, height);
+    context.translate(0, 0);
+    context.fillStyle = "rgb(0,0,0)";
+    context.fill();
+
+    // draw the lights
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        var x = j * 55 + 265;
+        var y = i * 55 + 105;
+        context.fillStyle = "rgb(65, 65, 65)";
+        if (lights[i][j])
+          context.fillStyle = "rgb(255, 255, 255)";
+        context.fillRect(x, y, 50, 50);
+      }
+    }
+      
+    if (hasWon) {
+      context.fillStyle = '#fff';
+      context.font = 'normal bold 16px sans-serif';
+      context.textBaseline = 'bottom';
+      context.fillText('You win!', 367, 75);
+    }
+    
+    context.restore();
+  };
+
+}(window.game = window.game || {}));
